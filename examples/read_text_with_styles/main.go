ffb283cb6821443b67ddcf67ec2235b70119ee7b
kvii <13721952202@163.com>
2026-05-22 11:01:40 +0800

    feat: change module's name.

--- examples/read_text_with_styles/main.go
+++ examples/read_text_with_styles/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/ledongthuc/pdf"
+	"github.com/kvii/pdf"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/ledongthuc/pdf
+module github.com/kvii/pdf
 
 go 1.25.0
 
